43ccad88fa70c31baacbd4156be1b8f469223cf2
JD277 <jdam50002@gmail.com>
2026-08-21 20:27:55 -0400

    Fixing vision_controler's script

--- src/visioncontroller/app.cpp
+++ src/visioncontroller/app.cpp
@@ -10,14 +10,18 @@ int main() {
     ROI main_roi = {220, 85, 420, 285}; 
 
     std::cout << "VisionController creado exitosamente." << std::endl;
+    std::vector<std::vector<cv::Point>> red;
     while (true){
         vision.receive_image();
         if (vision.get_frame().empty()) {
             std::cerr << "No se pudo obtener imagen" << std::endl;
             continue; // Intentar capturar la imagen nuevamente
         }
-
+        red = vision.find_contours({{18, 157, 0}, {255, 255, 255}}, main_roi);
+        vision.draw_contours(red, main_roi, cv::Scalar(0, 0, 255)); // Dibujar contornos en rojo
         vision.draw_roi(main_roi, cv::Scalar(255, 0, 0)); // Dibujar ROI de prueba
+        
+        cv::imshow("Frame", vision.get_frame());
         char key = (char)cv::waitKey(1);
         if (key == 'q') { // Salir si se presiona 'Esc'
             break;
